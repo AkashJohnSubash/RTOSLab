30cd53797e6722115979a68f4a4ecf2250274fda
AkashJohn <akash31994@gmail.com>
2025-04-25 12:15:53 +0530

    cleanup extra whitespaces

--- Protocols/Task4/EDF/main/application.c
+++ Protocols/Task4/EDF/main/application.c
@@ -1,23 +1,23 @@
 #include <stdio.h>                  // Standard lib headers
 #include "application.h"            // Application headers
 #include "freertos/FreeRTOS.h"      // Kernel headers
-#include "freertos/task.h" 
+#include "freertos/task.h"
 #include <rom/ets_sys.h>
 
 // -----------------------------Function declarations--------------------------
 void app_main(void)
-{   
+{
     // Generate init delay
     printf("\n[INFO] Application entry point found\r");
     pubTCB_t pTask_arr[APP_PER_TASKS] = {{0}};
     pubTCB_t apTask_arr[APP_APER_TASKS] = {{0}};
-    // user configures public periodic and aperiodic TCB 
+    // user configures public periodic and aperiodic TCB
     reg_periodic_tasks(pTask_arr);
     reg_aperiodic_tasks(apTask_arr);
     // initialize the EDF scheduler
     EdfScheduleInit(pTask_arr, apTask_arr);
     EdfScheduleStart();
-    // collect schedule and then delete tasks   
+    // collect schedule and then delete tasks
     vTaskDelay(pdMS_TO_TICKS(4000));
     EdfDeleteAllTasks();
     logToSerial(pTask_arr, apTask_arr);
@@ -27,8 +27,8 @@ void app_main(void)
 // ------------------------User function definitions-----------------------
 
 BaseType_t reg_periodic_tasks(pubTCB_t* pTask_arr)
-{    
-    // register the public periodic TCB 
+{
+    // register the public periodic TCB
     pTask_arr[0].phaseMs = 100;
     pTask_arr[0].WcetMs = 100;
     pTask_arr[0].periodMs = 400;
@@ -47,15 +47,15 @@ BaseType_t reg_periodic_tasks(pubTCB_t* pTask_arr)
     pTask_arr[2].WcetMs = 100;
     pTask_arr[2].periodMs = 400;
     pTask_arr[2].relDeadlineMs = 400;
-	pTask_arr[2].taskNumber = 7;	
+	pTask_arr[2].taskNumber = 7;
     pTask_arr[2].taskCallbackFunction = app_pTask3;
-    
+
     return ESP_OK;
 }
 
 BaseType_t reg_aperiodic_tasks(pubTCB_t* apTask_arr)
-{    
-    // register the public aperiodic TCB 
+{
+    // register the public aperiodic TCB
     apTask_arr[0].WcetMs = 100;
     apTask_arr[0].relArrivalMs = 200;
 	apTask_arr[0].taskNumber = 14;
@@ -65,58 +65,58 @@ BaseType_t reg_aperiodic_tasks(pubTCB_t* apTask_arr)
     apTask_arr[1].relArrivalMs = 800;
 	apTask_arr[1].taskNumber = 15;
     apTask_arr[1].taskCallbackFunction = app_apTask2;
-    
+
     return ESP_OK;
 }
 
 // periodic task1
 void app_pTask1(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // periodic task2
 void app_pTask2(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // periodic task3
 void app_pTask3(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // periodic task4
 void app_pTask4(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // aperiodic task1
 void app_apTask1(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // aperiodic task2
 void app_apTask2(int WcetUs)
-{   
+{
     // Replace busy waiting function with user code
-    busyWaitLoop(WcetUs); 
+    busyWaitLoop(WcetUs);
 }
 
 // keep CPU busy for the WCET duration
 void busyWaitLoop(int WcetUs)
-{   
+{
     // split into smaller chunks for preemtion to work correctly
     for(int i = 0; i < NUM_SLICES; i++)
     {
         ets_delay_us((WcetUs)/NUM_SLICES);
     }
-}    
+}
--- Protocols/Task4/EDF/main/logger.c
+++ Protocols/Task4/EDF/main/logger.c
@@ -1,9 +1,9 @@
 #include "application.h"            // Application headers
-#include "edf_scheduler.h"  
+#include "edf_scheduler.h"
 
 // -------------------Global variales------------------------
 int idx_gl = 0;
-int taskLog[LOG_NUM_EVNTS][LOG_NUM_FIELDS] = {0}; 
+int taskLog[LOG_NUM_EVNTS][LOG_NUM_FIELDS] = {0};
 
 // --------------------Private function prototypes--------------------
 
@@ -13,7 +13,7 @@ void displ_task(void* Unused);
 
 // --------------------Function definitions-----------------------------
 
-/* Function invoked by kernel via traceTASK_SWITCHED_IN() macro 
+/* Function invoked by kernel via traceTASK_SWITCHED_IN() macro
    to log task instance switch in time */
 void prvRecordStartTimeTrace(BaseType_t xTaskNumber, BaseType_t xTCBNumber)
 {
@@ -24,38 +24,38 @@ void prvRecordStartTimeTrace(BaseType_t xTaskNumber, BaseType_t xTCBNumber)
 	}
 }
 
-/* Function invoked by kernel via traceTASK_SWITCHED_OUT() macro 
+/* Function invoked by kernel via traceTASK_SWITCHED_OUT() macro
    to log task instance switch in time */
 void prvRecordEndTimeTrace(BaseType_t xTaskNumber, BaseType_t xTCBNumber)
-{	
+{
 	if(idx_gl < LOG_NUM_EVNTS)
-	{	
-		taskLog[idx_gl][2] = esp_timer_get_time();	
+	{
+		taskLog[idx_gl][2] = esp_timer_get_time();
 		idx_gl++;
 	}
 }
 
 void logToSerial(pubTCB_t* pTask_arr, pubTCB_t* apTask_arr)
-{   
+{
     printf("\n [DEBUG] Metadata start");
     printf("\n [DEBUG] Task start time Us : %d", firstTaskUs);
     printf("\n [DEBUG] System start tick %d", sysStartTick);
 
-    for (int16_t i = 0; i < APP_PER_TASKS; i++) 
-    {  
+    for (int16_t i = 0; i < APP_PER_TASKS; i++)
+    {
         // print aperiodic meta data for plot
-        printf("\n Periodic Task : %d, arrival : %d, phase : %d, wcet : %d, deadline : %d", 
+        printf("\n Periodic Task : %d, arrival : %d, phase : %d, wcet : %d, deadline : %d",
         pTask_arr[i].taskNumber,
-        0, 
+        0,
         pTask_arr[i].phaseMs,
         pTask_arr[i].WcetMs,
         pTask_arr[i].relDeadlineMs);
     }
-    
-    for (int16_t i = 0; i < APP_APER_TASKS; i++) 
-    {  
+
+    for (int16_t i = 0; i < APP_APER_TASKS; i++)
+    {
         // print periodic metadata for plot
-        printf("\n Aperiodic Task : %d arrival : %d, phase : %d, wcet : %d, deadline : %d", 
+        printf("\n Aperiodic Task : %d arrival : %d, phase : %d, wcet : %d, deadline : %d",
         apTask_arr[i].taskNumber,
         apTask_arr[i].relArrivalMs,
         0,
@@ -69,23 +69,23 @@ void logToSerial(pubTCB_t* pTask_arr, pubTCB_t* apTask_arr)
 
 /* Print fixed number of tasks to serial output or file*/
 void displ_task(void* Unused)
-{   
+{
     while(1)
-    {    
+    {
         //Display buffer only once, when filled
         if(idx_gl == LOG_NUM_EVNTS)
-        { 
+        {
             printf("********\n");
             printf("\n[INFO] Schedule Data Gathered. Printing data......\n");
             for(int i = 0; i < LOG_NUM_EVNTS; i++)
                 printf("\nEvent No: %d, Task No: %d, Entry at: %d Us, Exit at: %d Us. Overflow at %d Us",
-                 i, 
-                 taskLog[i][LOG_TASK_IDX], 
-                 taskLog[i][LOG_ENTRY_IDX], 
-                 taskLog[i][LOG_EXIT_IDX], 
+                 i,
+                 taskLog[i][LOG_TASK_IDX],
+                 taskLog[i][LOG_ENTRY_IDX],
+                 taskLog[i][LOG_EXIT_IDX],
                  taskLog[i][LOG_OVRFLW_IDX]);
             printf("\n[INFO] Schedule printed!!!\n");
-            printf("\n********");                    
+            printf("\n********");
         }
         // Delete display task once complete
 		vTaskDelete(NULL);
